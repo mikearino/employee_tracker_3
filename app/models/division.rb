7350842e4d08d0012392c5a5af9d1f3ac7d22e1b
Student <example@example.com>
2019-08-07 11:46:45 -0700

    add more employee functionality and validation to division

--- app/models/division.rb
+++ app/models/division.rb
@@ -1,3 +1,5 @@
 class Division < ApplicationRecord
   has_many :employees, dependent: :destroy
+  validates :name, presence: true
+  validates_length_of :name, maximum: 20
 end
--- spec/models/division_spec.rb
+++ spec/models/division_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 describe Division do
   it {  should have_many(:employees)}
+  it { should validate_presence_of :name }
+  it { should validate_length_of(:name).is_at_most(20) }
 end
